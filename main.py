199f26d1263a29da7642cd5d1243b4ce51c7a47b
cheshire <coren114@gmail.com>
2021-08-05 17:21:54 -0500

    dibujando hangman

--- main.py
+++ main.py
@@ -1,39 +1,7 @@
 from random import randrange
 import os
 from typing import IO
-# comprehension 
-def getRandomWord():
-    try:
-        names = []
-        with open('./archivos/data.txt', 'r', encoding='utf-8') as f: 
-            names = [name.replace('\n','') for name in f]
-            randname = names[randrange(len(names))]
-        return randname
-    except IOError:
-        print(IOError)
-        return 0
-    
-def show(lives,letters,name):
-    os.system('cls')
-    print('Adivina la palabra!')
-    print('vidas: ',lives)
-    if len(letters) > 0:
-        flag = True
-        for l in name:
-            for j in range(len(letters)):
-                if l == letters[j]:
-                    print(l,end=' ')
-                    flag= False
-                    break
-            if flag:
-                print('_',end=' ')
-            flag = True
-    else:
-        for i in name:
-            print('_', end=' ')
-    print()
-def hangman(score):
-    strhangman = [
+strhangman = [
         '''
           
           
@@ -134,6 +102,38 @@ def hangman(score):
            =======
         '''
     ]
+# comprehension 
+def getRandomWord():
+    try:
+        names = []
+        with open('./archivos/data.txt', 'r', encoding='utf-8') as f: 
+            names = [name.replace('\n','') for name in f]
+            randname = names[randrange(len(names))]
+        return randname
+    except IOError:
+        print(IOError)
+        return 0
+    
+def show(lives,letters,name):
+    os.system('cls')
+    print('Adivina la palabra!')
+    print('vidas: ',lives)
+    if len(letters) > 0:
+        flag = True
+        for l in name:
+            for j in range(len(letters)):
+                if l == letters[j]:
+                    print(l,end=' ')
+                    flag= False
+                    break
+            if flag:
+                print('_',end=' ')
+            flag = True
+    else:
+        for i in name:
+            print('_', end=' ')
+    print()
+def hangman(score):
     print(strhangman[(10-score)])
 
 def main ():
@@ -149,7 +149,7 @@ def main ():
         hangman(lives)
         try:
             lett = input('Ingresa una letra: ')
-            if len(lett) > 1 and (not lett.isalpha()):
+            if (len(lett) == 1 and (not lett.isalpha())) or len(lett) > 1:
                 raise ValueError('Se tiene que ingresar solo una letra')
             
             if lett in name:
